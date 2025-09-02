cbcea4b0f43ce6cf51ca9b5211fa86689c777fb6
Woshieeeee <anndelle123@gmail.com>
2025-09-02 22:54:52 +0800

    Delete BARANGAY LONGOS directory

--- BARANGAY LONGOS/js/about.js
+++
@@ -1,9 +0,0 @@
-// Toggle mobile menu
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-link');
-  const navMenu = document.getElementById('menu');
-
-  menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-  });
-});
